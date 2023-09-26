9ff9cc8a5d5c5d0c5872633e4df9d42fe529ea62
rahul <rahul.singh@almond.solutions>
2023-09-26 21:49:30 +0530

    Initial commit

---
+++ lib/live_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:zego_uikit_prebuilt_live_streaming/zego_uikit_prebuilt_live_streaming.dart';
+
+class LivePage extends StatelessWidget {
+  final String liveID;
+  final bool isHost;
+
+  const LivePage({Key? key, required this.liveID, this.isHost = false}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ZegoUIKitPrebuiltLiveStreaming(
+        appID: 1,// Fill in the appID that you get from ZEGOCLOUD Admin Console.
+        appSign: "yourAppSign",// Fill in the appSign that you get from ZEGOCLOUD Admin Console.
+        userID: 'user_id',
+        userName: 'user_name',
+        liveID: liveID,
+        config: isHost
+            ? ZegoUIKitPrebuiltLiveStreamingConfig.host()
+            : ZegoUIKitPrebuiltLiveStreamingConfig.audience(),
+      ),
+    );
+  }
+}
